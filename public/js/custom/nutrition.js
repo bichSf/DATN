a4cf76981cb450a47cb54595354fe9a26ad905d3
Tran Bich <tranbichbk@gmail.com>
2020-12-29 16:29:18 +0700

    destroy nutrition

--- public/js/custom/nutrition.js
+++ public/js/custom/nutrition.js
@@ -8,5 +8,36 @@ let nutritionFunction = (function () {
 $(document).ready(function () {
     $('select[name=table_type], select[name=survey_id]').on('change', function () {
         $('#form-statistical-population').submit();
-    })
+    });
+
+    $('.parent-checkbox').on('click', function () {
+        $('.sub-checkbox').prop('checked', $(this).is(":checked"))
+    });
+
+    $('.sub-checkbox').on('click', function () {
+        $('.parent-checkbox').prop('checked', $('.nutrition-management').find('.sub-checkbox:not(:checked)').length <= 0)
+    });
+
+    $('.delete-item').on('click', function () {
+        $('#form-delete-item').attr('action', window.location.origin + '/nutrition/delete/' + $(this).data('id'));
+    });
+
+    $('.btn-delete-multi').on('click', function () {
+        if ($('.nutrition-management').find('.sub-checkbox:checked').length <= 0) {
+            toastr.error('Bạn chưa chọn bản ghi nào.');
+        } else {
+            $('#modal-delete').modal('show');
+            $('#form-delete-item').attr('form-data', 'delete-multiple');
+        }
+    });
+
+    $('.button-delete').on('click', function () {
+        $(this).attr("disabled", true);
+        let attr = $('#form-delete-item').attr('form-data');
+        if (typeof attr !== typeof undefined && attr !== false) {
+            $('#delete-multi-record').submit();
+        } else {
+            $('#form-delete-item').submit();
+        }
+    });
 });
